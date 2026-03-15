e51f744ed3f7c7a4472ee1393584cebe57ddefda
Anthony Clerici <anthony@clerici.me>
2026-03-15 12:44:54 -0700

    test

--- charts/crowdsec/files/docker-start-custom.sh
+++ charts/crowdsec/files/docker-start-custom.sh
@@ -37,12 +37,12 @@ if istrue "$CI_TESTING"; then
     echo "githubciXXXXXXXXXXXXXXXXXXXXXXXX" >/etc/machine-id
 fi
 
-#- DEFAULTS -----------------------#
+#= DEFAULTS =======================#
 
 export CONFIG_FILE="${CONFIG_FILE:=/etc/crowdsec/config.yaml}"
 export CUSTOM_HOSTNAME="${CUSTOM_HOSTNAME:=localhost}"
 
-#- HELPER FUNCTIONS ----------------#
+#= HELPER FUNCTIONS ================#
 
 # csv2yaml <string>
 # generate a yaml list from a comma-separated string of values
@@ -219,7 +219,7 @@ if ( isfalse "$USE_TLS" || [ "$CLIENT_CERT_FILE" = "" ] ); then
     fi
 fi
 
-# ----------------
+# ================
 
 conf_set_if "$LOCAL_API_URL" '.url = strenv(LOCAL_API_URL)' "$lapi_credentials_path"
 
